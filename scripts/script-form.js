42531b364946d202b8989ff39da1a0a7adc54e13
Adan Rodriguez <adan.rodriguez.fusta@gmail.com>
2022-11-02 13:34:48 -0300

    ✨ Add defer attribute to scripts and horizontal scroll to table

--- scripts/script-form.js
+++ scripts/script-form.js
@@ -3,13 +3,33 @@ let formElement = document.getElementById("form");
 const getFormData = (e) => {
   e.preventDefault();
 
-  let formData = new FormData(formElement);
+  console.log("Name:", e.target.name.value);
+  console.log("Email:", e.target.email.value);
+  console.log("Comment:", e.target.comment.value);
 
-  for (let data of formData) {
-    console.log(data[0] + ": " + data[1]);
-  }
+  // let formData = new FormData(formElement);
 
-  formElement.reset();
+  // for (let data of formData) {
+  //   console.log(data[0] + ": " + data[1]);
+  // }
+
+  // formElement.reset();
 };
 
 formElement.addEventListener("submit", getFormData);
+
+// email.onblur = function () {
+//   if (!email.value.includes("@")) {
+//     // not email
+//     email.classList.add("invalid");
+//     error.innerHTML = "Por favor introduzca un correo válido.";
+//   }
+// };
+
+// email.onfocus = function () {
+//   if (this.classList.contains("invalid")) {
+//     // quitar la  indicación "error", porque el usuario quiere reintroducir algo
+//     this.classList.remove("invalid");
+//     error.innerHTML = "";
+//   }
+// };
